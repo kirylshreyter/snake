ed7c3e13cbc2348075d0ae5e97ffaaa41d63ab77
Admin_\kshreyter <kirylshreyter@gmail.com>
2017-08-07 01:27:52 +0300

    no message

--- application.js
+++ application.js
@@ -1,27 +1,27 @@
-var mainInterval;
-var eatInterval;
-var staticWidth = 5;
-var staticHeight = 5;
-var staticMoveSpeed = 1500;
-var staticCells;
-var currentPosition;
-var eatPosition;
+var stepInterval;
+var foodCreationInterval;
+var defaultWidth = 5;
+var defaultHeight = 5;
+var defaultSpeed = 1500;
+var allGridCells;
+var currentHeadPosition;
+var foodPosition;
 var direction = 'right';
-var eatCounter = 0;
+var foodCounter = 0;
 var fullSnake = [];
 var score;
 
 
 function createEat() {
-    if (eatCounter != 1) {
-        eatPosition = [generate(0, staticWidth), generate(0, staticHeight)];
+    if (foodCounter != 1) {
+        foodPosition = [generate(0, defaultWidth), generate(0, defaultHeight)];
         for (var fullSnakeIndexer = 0; fullSnakeIndexer < fullSnake.length; fullSnakeIndexer++) {
-            while (fullSnake[fullSnakeIndexer][0] == eatPosition[0] && fullSnake[fullSnakeIndexer][1] == eatPosition[1]) {
-                eatPosition = [generate(0, staticWidth), generate(0, staticHeight)];
+            while (fullSnake[fullSnakeIndexer][0] == foodPosition[0] && fullSnake[fullSnakeIndexer][1] == foodPosition[1]) {
+                foodPosition = [generate(0, defaultWidth), generate(0, defaultHeight)];
             }
         }
-        findCellAndPaintOver(eatPosition, 'FF7F50');
-        eatCounter = 1;
+        findCellAndPaintOver(foodPosition, 'FF7F50');
+        foodCounter = 1;
     }
 }
 
@@ -54,20 +54,20 @@ function appendChild(parent, appenderStyle) {
 
 function createNewGrid() {
     if (document.getElementById('width').value) {
-        staticWidth = document.getElementById('width').value;
+        defaultWidth = document.getElementById('width').value;
     }
     if (document.getElementById('height').value) {
-        staticHeight = document.getElementById('height').value;
+        defaultHeight = document.getElementById('height').value;
     }
     if (document.getElementById('moveSpeed').value != 'Please, select speed') {
-        staticMoveSpeed = document.getElementById('moveSpeed').value * 500;
+        defaultSpeed = document.getElementById('moveSpeed').value * 500;
     }
-    document.getElementById('grid').appendChild(createGrid(staticWidth, staticHeight));
-    currentPosition = [generate(0, staticWidth), generate(0, staticHeight)];
-    fullSnake.unshift(currentPosition);
+    document.getElementById('grid').appendChild(createGrid(defaultWidth, defaultHeight));
+    currentHeadPosition = [generate(0, defaultWidth), generate(0, defaultHeight)];
+    fullSnake.unshift(currentHeadPosition);
     setStyleDisplayToElement('grid', 'block');
     setStyleDisplayToElement('dialog', 'none');
-    staticCells = getElementsByClassName('cell');
+    allGridCells = getElementsByClassName('cell');
     document.addEventListener('keydown', changeDirection);
     setTimeout(start, 2000);
 }
@@ -82,8 +82,8 @@ function setStyleDisplayToElement(elementId, value) {
 }
 
 function findCellByCoordinates(position) {
-    for (var staticCellsIndexer = 0; staticCellsIndexer < staticCells.length; staticCellsIndexer++) {
-        var curCell = staticCells[staticCellsIndexer];
+    for (var staticCellsIndexer = 0; staticCellsIndexer < allGridCells.length; staticCellsIndexer++) {
+        var curCell = allGridCells[staticCellsIndexer];
         if ((curCell.getAttribute('x') == position[0] & curCell.getAttribute('y') == position[1])) {
             return curCell;
         }
@@ -91,93 +91,110 @@ function findCellByCoordinates(position) {
 }
 
 function start() {
-    eatInterval = setInterval(createEat, 500);
-    mainInterval = setInterval(getNextDefaultPosition, staticMoveSpeed);
+    foodCreationInterval = setInterval(createEat, 500);
+    stepInterval = setInterval(getNextDefaultPosition, defaultSpeed);
 }
 
-function compareTwoArrays(firstArray, secondArray) {
-    return firstArray.length == secondArray.length && firstArray.every(function (element, index) {
-        return element === secondArray[index];
+function compareTwoPositions(firstPosition, secondPosition) {
+    return firstPosition.length == secondPosition.length && firstPosition.every(function (element, index) {
+        return element === secondPosition[index];
     });
 }
 
-function checkIfBrake(param1, param2) {
+function checkIfBrake(position) {
     for (var fullSnakeIndexer = 0; fullSnakeIndexer < fullSnake.length; fullSnakeIndexer++) {
-        if (fullSnake[fullSnakeIndexer][0] == param1 && fullSnake[fullSnakeIndexer][1] == param2) {
-            clearInterval(eatInterval);
-            clearInterval(mainInterval);
+        if (fullSnake[fullSnakeIndexer][0] == position[0] && fullSnake[fullSnakeIndexer][1] == position[1]) {
+            clearInterval(foodCreationInterval);
+            clearInterval(stepInterval);
             score = fullSnake.length;
             alert('GAME OVER, YOUR SCORE ' + score);
         }
     }
 }
 
-function getNextDefaultPosition() {
-    var pos = currentPosition;
 
-    function compareTwoPositions() {
-        if (compareTwoArrays(pos, eatPosition)) {
-            fullSnake.unshift(eatPosition);
-            eatCounter = 0;
+function getNextDefaultPosition() {
+    var position = currentHeadPosition;
+
+    function increaseSnakeLength() {
+        if (compareTwoPositions(position, foodPosition)) {
+            fullSnake.unshift(foodPosition);
+            foodCounter = 0;
         }
+    }
+
+    function stepLeft() {
+        if (currentHeadPosition[0] - 1 >= 0) {
+            currentHeadPosition = [currentHeadPosition[0] - 1, currentHeadPosition[1]];
+            increaseSnakeLength();
+        } else {
+            currentHeadPosition = [defaultWidth - 1, currentHeadPosition[1]];
+            increaseSnakeLength();
+        }
+        move(currentHeadPosition);
+    }
+
+    function stepTop() {
+        if (currentHeadPosition[1] - 1 >= 0) {
+            currentHeadPosition = [currentHeadPosition[0], currentHeadPosition[1] - 1];
+            increaseSnakeLength();
+        } else {
+            currentHeadPosition = [currentHeadPosition[0], defaultHeight - 1];
+            increaseSnakeLength();
+        }
+        move(currentHeadPosition);
+    }
+
+    function stepRight() {
+        if (currentHeadPosition[0] + 1 < defaultWidth) {
+            currentHeadPosition = [currentHeadPosition[0] + 1, currentHeadPosition[1]];
+            increaseSnakeLength();
+        } else {
+            currentHeadPosition = [0, currentHeadPosition[1]];
+            increaseSnakeLength();
+        }
+        move(currentHeadPosition);
+    }
+
+    function stepDown() {
+        if (currentHeadPosition[1] + 1 < defaultHeight) {
+            currentHeadPosition = [currentHeadPosition[0], currentHeadPosition[1] + 1];
+            increaseSnakeLength();
+        } else {
+            currentHeadPosition = [currentHeadPosition[0], 0];
+            increaseSnakeLength();
+        }
+        move(currentHeadPosition);
     }
 
     switch (direction) {
         case 'left':
-            if (currentPosition[0] - 1 >= 0) {
-                currentPosition = [currentPosition[0] - 1, currentPosition[1]];
-                compareTwoPositions();
-            } else {
-                currentPosition = [staticWidth - 1, currentPosition[1]];
-                compareTwoPositions();
-            }
-            move(currentPosition);
+            stepLeft();
             break;
         case 'top':
-            if (currentPosition[1] - 1 >= 0) {
-                currentPosition = [currentPosition[0], currentPosition[1] - 1];
-                compareTwoPositions();
-            } else {
-                currentPosition = [currentPosition[0], staticHeight - 1];
-                compareTwoPositions();
-            }
-            move(currentPosition);
+            stepTop();
             break;
         case 'right':
-            if (currentPosition[0] + 1 < staticWidth) {
-                currentPosition = [currentPosition[0] + 1, currentPosition[1]];
-                compareTwoPositions();
-            } else {
-                currentPosition = [0, currentPosition[1]];
-                compareTwoPositions();
-            }
-            move(currentPosition);
+            stepRight();
             break;
         case 'down':
-            if (currentPosition[1] + 1 < staticHeight) {
-                currentPosition = [currentPosition[0], currentPosition[1] + 1];
-                compareTwoPositions();
-            } else {
-                currentPosition = [currentPosition[0], 0];
-                compareTwoPositions();
-            }
-            move(currentPosition);
+            stepDown();
             break;
     }
 }
 
 function changeDirection(e) {
     switch (e.keyCode) {
-        case 37:    // left
+        case 37:
             direction = 'left';
             break;
-        case 38:   // top
+        case 38:
             direction = 'top';
             break;
-        case 39:   // right
+        case 39:
             direction = 'right';
             break;
-        case 40:   // down
+        case 40:
             direction = 'down';
             break;
     }
@@ -185,9 +202,9 @@ function changeDirection(e) {
 
 function move(position) {
     findCellAndPaintOver(fullSnake.pop(), '#708090');
-    checkIfBrake(position[0], position[1]);
+    checkIfBrake(position);
     fullSnake.unshift(position);
-    colorFields(fullSnake);
+    paintCells(fullSnake);
 }
 
 function findCellAndPaintOver(position, color) {
@@ -195,7 +212,7 @@ function findCellAndPaintOver(position, color) {
     cell.style.backgroundColor = color;
 }
 
-function colorFields(snake) {
+function paintCells(snake) {
     findCellAndPaintOver(snake[0], 'red');
     for (var snakeIndexer = 1; snakeIndexer < snake.length; snakeIndexer++) {
         findCellAndPaintOver(snake[snakeIndexer], '00008b');
